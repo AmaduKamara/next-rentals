fe3917058c55daddda28d5c12bea778dd4f17213
Amkam <amaduskamara@gmail.com>
2022-06-08 09:27:06 +0000

    Add pages based on nav items

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,23 +1,34 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Header = () => {
-  const user = true;
+  const user = false;
+  const router = useRouter();
+
   return (
     <header className="py-5 bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
-          <a className="font-bold text-2xl">
+          <a className="font-bold text-2xl text-gray-700">
             Next~<span className="text-red-700">Rentals</span>{" "}
           </a>
         </Link>
         <nav className="flex items-center">
           <ul className="flex items-center">
-            <li className="mx-5 text-lg hover:text-red-700">
+            <li
+              className={`mx-5 text-lg hover:text-red-700 ${
+                router.pathname === "/cart" ? "text-red-700" : ""
+              }`}
+            >
               <Link href="/cart">Cart</Link>
             </li>
             {user && (
-              <li className="mx-5 text-lg hover:text-red-700">
+              <li
+                className={`mx-5 text-lg hover:text-red-700 ${
+                  router.pathname === "/create" ? "text-red-700" : ""
+                }`}
+              >
                 <Link href="/create">Create</Link>
               </li>
             )}
@@ -25,7 +36,11 @@ const Header = () => {
           <ul className="flex items-center">
             {user ? (
               <>
-                <li className="mx-5 text-lg hover:text-red-700">
+                <li
+                  className={`mx-5 text-lg hover:text-red-700 ${
+                    router.pathname === "/account" ? "text-red-700" : ""
+                  }`}
+                >
                   <Link href="/account">Account</Link>
                 </li>
                 <li className="ml-5 text-lg hover:text-red-700">
@@ -34,10 +49,18 @@ const Header = () => {
               </>
             ) : (
               <>
-                <li className="mx-5 text-lg hover:text-red-700">
+                <li
+                  className={`mx-5 text-lg hover:text-red-700 ${
+                    router.pathname === "/login" ? "text-red-700" : ""
+                  }`}
+                >
                   <Link href="/login">Login</Link>
                 </li>
-                <li className="ml-5 text-lg hover:text-red-700">
+                <li
+                  className={`mx-5 text-lg hover:text-red-700 ${
+                    router.pathname === "/signup" ? "text-red-700" : ""
+                  }`}
+                >
                   <Link href="/signup">Signup</Link>
                 </li>
               </>
